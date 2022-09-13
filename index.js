b89aaaaf507379398ebc7b797b101a4d4b7120c2
Taohid <taohid32@gmail.com>
2022-09-13 17:01:22 +0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,98 @@
+const { promisify } = require('util');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const delay = promisify(setTimeout);
+const pageScroll = require('./page-scroll');
+
+var newsItems = [
+    {
+        "href": "https://en.prothomalo.com/bangladesh/53w9yack4a",
+        "headline": "Bangladesh loses remittance worth Tk 750b in hundi: CID",
+        "date": "08 Sep 2022"
+    }
+  ];
+
+  
+(async () => {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.goto('https://en.prothomalo.com/topic/Money-laundering')
+    await loadAllContent(page)
+    var newsItems = await scrapNewsMeta(page)
+    await page.close()  
+    var data = await scrapNewsDetails(browser, newsItems)
+    saveToJSON(data)
+    await browser.close()
+})().catch((error) => {
+    console.error("the message is " + error.message)
+})
+
+
+async function loadAllContent(page) {
+    console.log('Load More Started')
+    while(await page.$('.load-more-content') !== null) {
+        await page.click(".load-more-content");
+        await delay(800);
+    }
+    console.log('Load More Done')
+}
+
+async function scrapNewsMeta(page) {
+    console.log('Meta Data Scrapping started')
+    const data = await page.$$eval('.left_image_right_news', async function(options) {
+        return options.map(function(option) {
+            return {
+                'href': option.querySelector('.card-with-image-zoom').getAttribute('href'),
+                'headline': option.querySelector('.headline-title').innerText,
+                'date': option.querySelector('.published-time').innerText,
+            }
+        }) 
+    })
+    console.log('Meta Data Scrapping done')
+    return data
+}
+
+async function scrapNewsDetails(browser, newsItems) {
+    console.log('Details Scrapping started')
+
+    return await Promise.all(newsItems.map(async newsMeta => {
+
+        const page = await browser.newPage()
+        await page.goto(newsMeta.href, {waitUntil: 'load', timeout: 0})
+        await pageScroll.autoScroll(page, 300)
+
+        newsMeta['headline'] = await page.$eval('.storytitleInfo-m__story-headline__30dXX', function(option) {
+            return option.innerText
+        })
+        newsMeta['content'] = await page.$$eval('p', function(options) {
+            return options.map(function(option) {
+                return option.innerText
+            })
+        })
+
+        if(await page.$('.qt-image') !== null) {
+            newsMeta['image'] =  await page.$eval('.qt-image', function(option) {
+                return option.src
+            })
+        }
+
+        await page.close();
+            
+    })).then(result => {
+        console.log('Total ' + newsItems.length + ' Item Found')
+        return newsItems
+    }).catch(err => {
+        console.log(err)
+        return newsItems
+    })
+}
+
+function saveToJSON(data) {
+    const stringData = JSON.stringify(data, null, 4);
+    fs.writeFile('data.json', stringData, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    });
+}
---
+++ page-scroll.js
@@ -0,0 +1,37 @@
+const autoScroll = async function(page, maxScroll) {
+     await page.setViewport({
+        width: 1200,
+        height: 800
+    })
+    await _scroll(page, maxScroll);
+    console.log('Auto scroll completed')
+}
+
+
+async function _scroll(page, maxScroll){
+    
+    await page.evaluate(async () => {
+
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                // console.log('scrollHeight - window.innerHeight: ' + scrollHeight - window.innerHeight)
+
+                if(totalHeight >= 300){
+                    clearInterval(timer);
+                    resolve();
+                }
+
+            }, 200);
+        });
+    });
+}
+
+module.exports = {
+    autoScroll,
+}
